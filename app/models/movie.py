406fe3938512951d358010081758347b65c45d21
banchinche <andrew.hrimov@gmail.com>
2021-07-05 12:02:13 +0300

    Models update

--- app/models/movie.py
+++ app/models/movie.py
@@ -12,25 +12,24 @@ class Movie(db.Model):
     rate = db.Column(SMALLINT, CheckConstraint('rate > 0 and rate <= 10'), nullable=False)
     year = db.Column(SMALLINT, CheckConstraint('year > 1880'), nullable=False)
     description = db.Column(TEXT)
-
+    poster = db.Column(VARCHAR(255), nullable=False)
     director_id = db.Column(INTEGER, nullable=False)
     user_id = db.Column(INTEGER, nullable=False)
-    poster_id = db.Column(INTEGER, nullable=False)
     ForeignKeyConstraint(
-        ['director_id', 'user_id', 'poster_id'],
-        ['Director.id', 'User.id', 'Poster.id'],
+        ['director_id', 'user_id'],
+        ['Director.id', 'User.id'],
         onupdate="CASCADE"
     )
     created = db.Column(TIMESTAMP, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, name, rate, year, description, d_id, u_id, p_id, date):
+    def __init__(self, name, rate, year, description, image_link, d_id, u_id, date):
         self.name = name
         self.rate = rate
         self.year = year
         self.description = description
+        self.poster = image_link
         self.director_id = d_id
         self.user_id = u_id
-        self.poster_id = p_id
         self.created = date
 
     def __repr__(self):
